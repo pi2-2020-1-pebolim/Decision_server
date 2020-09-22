9d4f267e0b5684d3fd97ad5df6fa997f5d110074
João Pedro Pereira <jplpereira@gmail.com>
2020-09-22 14:05:41 -0300

    feat: identifies ball position
    Signed-off-by: Joao Pedro Pereira  <jplpereira@gmail.com>

--- src/controllers/image_controller.py
+++ src/controllers/image_controller.py
@@ -31,7 +31,7 @@ class ImageController:
 
         ret, thresh = cv.threshold(mask, 255, 255, 255)
 
-        contours, hierarchy= cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
         sorted_contours= sorted(contours, key=cv.contourArea, reverse= True)
 
@@ -49,3 +49,39 @@ class ImageController:
         encoded_gray_image = Base64Convertion().encode_base_64(gray_image.tobytes()).decode('ascii')
         return encoded_gray_image
 
+    def retrieveBallCoordinates(self, frame):
+        # define the lower and upper boundaries of the "green"
+        # ball in the HSV color space
+        # ball RGB color (150, 209, 119)
+        greenLower = (29, 86, 6)
+        greenUpper = (64, 255, 255)
+
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        frame = imutils.resize(frame, width=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        # construct a mask for the color "green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        return center
---
+++ tests_vision_compute/coordinates_test.py
@@ -0,0 +1,62 @@
+# reference: https://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
+
+# import the necessary packages
+from collections import deque
+import numpy as np
+import cv2
+import imutils
+import time
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+
+# initialize the list of tracked points, the frame counter,
+# and the coordinate deltas
+max_points_object = 10
+pts = deque(maxlen = max_points_object)
+counter = 0
+(dX, dY) = (0, 0)
+direction = ""
+
+# read the frame
+frame = cv2.imread('pebolim_teste_bola_verde.jpg', cv2.IMREAD_COLOR)
+
+# resize the frame, blur it, and convert it to the HSV
+# color space
+frame = imutils.resize(frame, width=600)
+blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+# construct a mask for the color "green", then perform
+# a series of dilations and erosions to remove any small
+# blobs left in the mask
+mask = cv2.inRange(hsv, greenLower, greenUpper)
+mask = cv2.erode(mask, None, iterations=2)
+mask = cv2.dilate(mask, None, iterations=2)
+
+# find contours in the mask and initialize the current
+# (x, y) center of the ball
+cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+center = None
+
+# only proceed if at least one contour was found
+if len(cnts) > 0:
+    # find the largest contour in the mask, then use
+    # it to compute the minimum enclosing circle and
+    # centroid
+    c = max(cnts, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
+    M = cv2.moments(c)
+    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    # only proceed if the radius meets a minimum size
+    ball_minimum_size = 1
+    if radius > ball_minimum_size:
+        # draw the circle and centroid on the frame,
+        # then update the list of tracked points
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        pts.appendleft(center)
+        print(center)
