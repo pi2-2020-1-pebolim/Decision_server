2f5ebb7cfdd465e7039ed273d16b0cefea959637
João Pedro Pereira <jplpereira@gmail.com>
2020-10-22 15:55:15 -0300

    feat: create player class

---
+++ src/model/player.py
@@ -0,0 +1,22 @@
+
+class Player:
+
+    def __init__(self, laneID, xPosition):
+        super().__init__()
+        self.laneID = laneID
+        self.xPosition = xPosition
+        self.yCenterPosition = 0
+        self.yMaxPosition = 0
+        self.yMinPosition = 0
+
+    def set_y_center_position(self, yCenterPosition):
+        self.yCenterPosition = yCenterPosition
+
+    def set_y_max_min_positions(self, movementLimit):
+        self.yMaxPosition = self.yCenterPosition + movementLimit
+        self.yMinPosition = self.yCenterPosition - movementLimit
+
+    def get_y_max_min_positions(self):
+        return [self.yMaxPosition, self.yMinPosition]
+ 
+    
